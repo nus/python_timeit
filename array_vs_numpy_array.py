988cab2c7ad1292f81e2f2e7081bfa276f59243e
Yota Ichino <www.irda@gmail.com>
2016-11-08 22:36:31 +0900

    Add sum function

--- array_vs_numpy_array.py
+++ array_vs_numpy_array.py
@@ -24,9 +24,23 @@ def for_each_ary():
 
     return ret
 
+print('Accessing array elements')
 print(timeit.timeit(for_each_np_ary, number=times))
 print(timeit.timeit(for_each_ary, number=times))
 
 # time_it$ python array_vs_numpy_array.py
 # 2.27572703362
 # 0.504191160202
+
+def sum_np_ary():
+    return np.sum(np_ary)
+
+def sum_ary():
+    return sum(ary)
+
+print('Sum of array elements')
+print(timeit.timeit(sum_np_ary, number=times))
+print(timeit.timeit(sum_ary, number=times))
+
+# 0.0825188159943
+# 0.0919990539551
